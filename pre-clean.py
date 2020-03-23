fe735d7354bf7d1c721c2850409815858669a35b
Iván Todorovich <ivan.todorovich@gmail.com>
2020-03-23 18:42:50 +0100

    [IMP] Reorganize module operations on config.py

--- pre-clean.py
+++ pre-clean.py
@@ -1,60 +1,47 @@
 import click
 import click_odoo
+from config import *
 
 
 @click.command()
 @click_odoo.env_options(default_log_level='error')
 def main(env):
     print('Pre cleaning started...')
-    modules_to_uninstall = [
-        'account_payment_transfer_account', 'louve_custom_account',
-        'account_cancel', 'account_deprecated',
-        'account_budget',
-        'pos_transfer_account',
-        'account_finance_report_xlsx', 'smile_base', 'smile_upgrade',
-        'louve_custom_product', 'louve_custom_email',
-        'louve_welcome_email',
-        'account_asset', 'web_sheet_full_width',
 
-        'product_to_print',      # renamed to product_print_category
-        'pos_return_order',      # renamed to pos_order_return
-        'account_bank_statement_summary',  # renamed to
-                                 # account_bank_statement_reconciliation_report
-        'pos_product_barcodes',  # Merged with coop_point_of_sale
+    modules_to_uninstall = (
+        + MODULES_TO_UNINSTALL
+        + MODULES_PENDING_MIGRATION
+        + [i[0] for i in MODULES_TO_REPLACE]
+    )
 
-        # Modules to Migrate.
-        'web_widget_image_webcam', 'coop_produce',
-        'date_search_extended',
-        'product_to_scale_bizerba', 'mass_editing',
-        'pos_session_summary',
-        'account_bank_statement_reconcile_option',
-        'pos_automatic_cashdrawer', 'account_reconcile_pos_payments',
-        'pos_payment_terminal_return', 'account_bank_reconciliation_rule',
-        'account_bank_statement_import_caisse_epargne',
-        'account_bank_statement_import_ofx', 'account_invoice_refund_option',
-        'account_mass_reconcile', 'account_payment_confirm',
-        'account_payment_select_account', 'account_payment_term_restricted',
-        'account_reconcile_writeoff_improve', 'auto_backup',
-        'base_import_security_group', 'base_technical_features',
-        'email_attachment_custom', 'hw_cashlogy', 'pos_price_to_weight',
-        'purchase_operation_adjust', 'scheduler_error_mailer',
-        'stock_inventory_by_category', 'stock_inventory_xlsx',
-        'web_readonly_bypass',
-
-        # Moved to Enterprise in 12.0
-        'marketing_campaign',
-
-        # not needed in 12.0
-        'stock_expense_transfer', 'pos_report_total_vat_excluded',
-        'account_accountant', 'account_journal_inactive',
-    ]
     for module in modules_to_uninstall:
-        print('Uninstalling module: %s..' % module)
+        # Skip modules that are depended by others,
+        # because that might trigger a chain uninstall,
+        # removing modules we don't want to remove.
+        # These will be uninstalled by post-clean.
+        depended_by = env['ir.module.module'].search([
+            ('state', 'in', ['installed', 'to upgrade']),
+            ('dependencies_id', 'in', module)])
+        if depended_by:
+            print((
+                'Skipping module uninstall: %s, '
+                'because it\'s dependend by: %s'
+                ) % (module, depended_by.mapped('name')))
+            continue
+
         module_id = env['ir.module.module'].search([('name', '=', module)])
-        if module_id:
-            module_id.button_immediate_uninstall()
-        else:
+        if not module_id:
             print('Module not found: %s' % module)
+            continue
+
+        # Skip modules that are not installed
+        if module_id.state not in ['installed', 'to upgrade']:
+            print('Skipping module uninstall: %s. Not installed.' % module)
+            continue
+
+        # Uninstall module
+        print('Uninstalling module: %s..' % module)
+        module_id.button_immediate_uninstall()
 
     print('Pre cleaning finished successfully!')
 

f5da61fee218f84438a74029750640c8397aa901
Iván Todorovich <ivan.todorovich@gmail.com>
2020-04-01 18:32:28 +0200

    [IMP] Try to fix weird error in ir_act_window

--- migrate-all.sh
+++ migrate-all.sh
@@ -5,8 +5,6 @@ set -e
 docker-compose -f docker-compose.9.0.yml up --abort-on-container-exit
 docker-compose -f docker-compose.10.0.yml up --abort-on-container-exit
 docker-compose -f docker-compose.11.0.yml up --abort-on-container-exit
-
-# Openupgrade + Project addons
 docker-compose -f docker-compose.12.0.yml up --abort-on-container-exit
 
 # Standard Odoo + Project addons

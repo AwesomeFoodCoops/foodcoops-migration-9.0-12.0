11f6746e1edd159fd7a1b595df5698022e6c79e7
Ivàn Todorovich <ivan.todorovich@gmail.com>
2020-04-06 12:47:51 +0200

    [REM] Rem openupgrade from sources, since it's alreaday included in the new docker openugprade image

--- setup.py
+++ setup.py
@@ -17,39 +17,6 @@ if not os.path.exists(SOURCES):
     os.makedirs(SOURCES)
 
 
-print('Generating openupgrade repos.yml..')
-
-versions = ['9.0', '10.0', '11.0', '12.0']
-content = '\n'.join(['''
-openupgrade-{branch}:
-  defaults:
-    depth: 1
-  remotes:
-    origin: https://github.com/OCA/openupgrade.git
-  merges:
-    - origin {branch}
-  target: origin {branch}
-'''.format(branch=version) for version in versions])
-
-repos_file = os.path.join(SOURCES, 'openupgrade.yml')
-with open(repos_file, 'w+') as file:
-    file.write(content)
-
-
-print('Aggregatting openugprade requirements..')
-check_call(
-    [
-        'gitaggregate',
-        '--config',
-        repos_file,
-    ],
-    cwd=SOURCES,
-    stderr=sys.stderr,
-    stdout=sys.stdout,
-)
-os.remove(repos_file)
-
-
 print('Adding final project repositories')
 REPOSITORIES = os.path.join(SOURCES, 'repositories')
 if not os.path.exists(REPOSITORIES):

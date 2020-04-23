245ddde08e0bb4758a27365ad17f2c280eed87d8
Ivàn Todorovich <ivan.todorovich@gmail.com>
2020-04-23 12:44:50 +0200

    [FIX] Tests docker-compose files

--- clean.sh
+++ clean.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 set -e
 
+# Load db
 docker-compose -f docker-compose.load.yml down -v
+
+# Openupgrade
 docker-compose -f docker-compose.9.0.yml down -v
 docker-compose -f docker-compose.10.0.yml down -v
 docker-compose -f docker-compose.11.0.yml down -v
 docker-compose -f docker-compose.12.0.yml down -v
-docker-compose -f docker-compose.test.yml down -v
+
+# Tests
+docker-compose -f docker-compose.9.0-test.yml down -v
+docker-compose -f docker-compose.10.0-test.yml down -v
+docker-compose -f docker-compose.11.0-test.yml down -v
+docker-compose -f docker-compose.12.0-test.yml down -v

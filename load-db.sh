c38769db8c316be5a1abd2b9a83834a556ef30a2
Ivàn Todorovich <ivan.todorovich@gmail.com>
2020-04-30 17:26:33 +0200

    [ADD] load-db.sh script

---
+++ load-db.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+# Load dump.gz database automatically
+echo "Starting load container.."
+sudo docker-compose -f docker-compose.load.yml up -d
+
+if [[ -f backups/migrate.dump.gz ]]; then
+	echo "Loading backup from migrate.dump.gz.."
+	curl -F backup_file=@backups/migrate.dump.gz -F name=migrate -F master_pwd=admin -F copy=true http://localhost:8069/web/database/restore
+elif [[ -f backups/migrate.zip ]]; then
+	echo "Loading backup from migrate.zip.."
+	curl -F backup_file=@backups/migrate.zip -F name=migrate -F master_pwd=admin -F copy=true http://localhost:8069/web/database/restore
+fi
+
+echo "Shutting down load container.."
+sudo docker-compose -f docker-compose.load.yml down

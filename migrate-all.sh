793e7f71f98c08dd82ef8cbc8a4dce13dab7b7a4
Ivàn Todorovich <ivan.todorovich@gmail.com>
2020-05-04 14:12:48 +0200

    [IMP] Final step (to run outside of openugprade because of bugs)

--- migrate-all.sh
+++ migrate-all.sh
@@ -9,3 +9,4 @@ sudo docker-compose -f docker-compose.9.0.yml up --abort-on-container-exit
 sudo docker-compose -f docker-compose.10.0.yml up --abort-on-container-exit
 sudo docker-compose -f docker-compose.11.0.yml up --abort-on-container-exit
 sudo docker-compose -f docker-compose.12.0.yml up --abort-on-container-exit
+sudo docker-compose -f docker-compose.final.yml up --abort-on-container-exit

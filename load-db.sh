58984acd29f32dcc1719f76d93ded17f9446e95c
Ivàn Todorovich <ivan.todorovich@gmail.com>
2020-05-04 10:41:16 +0200

    Use wait-for-it.sh to load db

--- load-db.sh
+++ load-db.sh
@@ -5,6 +5,13 @@ set -e
 echo "Starting load container.."
 sudo docker-compose -f docker-compose.load.yml up -d
 
+# Wait for it
+./wait-for-it.sh --timeout=0 localhost:8069
+
+# Try to drop database
+curl -F name=migrate -F master_pwd=admin http://localhost:8069/web/database/drop
+
+# Upload new database
 if [[ -f backups/migrate.dump.gz ]]; then
 	echo "Loading backup from migrate.dump.gz.."
 	curl -F backup_file=@backups/migrate.dump.gz -F name=migrate -F master_pwd=admin -F copy=true http://localhost:8069/web/database/restore
